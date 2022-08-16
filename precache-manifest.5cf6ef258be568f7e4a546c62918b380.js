faa009853fdd38c3012e48437ffe128c04f7281c
julhoang <64108232+julhoang@users.noreply.github.com>
2022-08-16 19:13:19 +0700

    Updates

---
+++ precache-manifest.5cf6ef258be568f7e4a546c62918b380.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "00110b21a6aa753870a4",
+    "url": "/recap/static/css/main.1009cb24.chunk.css"
+  },
+  {
+    "revision": "00110b21a6aa753870a4",
+    "url": "/recap/static/js/main.00110b21.chunk.js"
+  },
+  {
+    "revision": "2da4881f6d936faa37df",
+    "url": "/recap/static/css/1.625d8624.chunk.css"
+  },
+  {
+    "revision": "2da4881f6d936faa37df",
+    "url": "/recap/static/js/1.2da4881f.chunk.js"
+  },
+  {
+    "revision": "761219bebe43c33ebd03",
+    "url": "/recap/static/js/runtime~main.761219be.js"
+  },
+  {
+    "revision": "ddc0b54d37c8eaca5e240ff3b7d7eb8c",
+    "url": "/recap/index.html"
+  }
+];
