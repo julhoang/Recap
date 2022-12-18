70d22a7049cbe39b29eb5d39b90ee7b577a67ea3
julhoang <64108232+julhoang@users.noreply.github.com>
2022-12-18 01:54:26 -0800

    Fix useEffect implementation: only pass data onto children components once API finish loading

--- src/pages/editor.js
+++ src/pages/editor.js
@@ -6,57 +6,58 @@ import "@yaireo/tagify/dist/tagify.css";
 import Navbar from "../editor-components/Navbar";
 import Info from "../editor-components/Info";
 import Content from "../editor-components/Content";
-import TextEditor from "../editor-components/TextEditor";
 
 /**
  * @constructor
- * @param: none
  * @returns the entire Editor page
  */
 
 export default function Editor() {
   const [book, setBook] = useState(undefined);
-  const [info, setInfo] = useState(undefined);
-  const id = window.location.search.replace("?id=", "");
+  const bookID = window.location.search.replace("?id=", "");
 
   // Purpose: retrieve book entry from Firebase
   const getBook = async () => {
-    const docRef = doc(db, "books", id);
+    const docRef = doc(db, "books", bookID);
     const docSnap = await getDoc(docRef);
 
     if (docSnap.exists()) {
+      console.log("found book from Firebase");
       setBook(docSnap.data());
     }
   };
 
   useEffect(() => {
     getBook();
-  }, []);
-
-  // TODO: implement TextEditor using SlateJS
+  }, []); // the [] will help this runs only once - after the page renders
 
   return (
     <div id="editor-main">
       {/* Navbar containing Back, Mark as Completed and More buttons */}
-      <Navbar
-        data={book}
-        id={id}
-      />
+      {book && (
+        <Navbar
+          data={book}
+          id={bookID}
+        />
+      )}
 
       {/* Book Info (cover img, title, author, rating, tags) Section */}
-      <Info
-        data={book}
-        id={id}
-        api={info}
-      />
-
-      {/* Text/Quotes Editor */}
-      <div id="text-editor-section">
-        <Content
-          data={book}
-          api={info}
+      {book && (
+        <Info
+          book={book}
+          bookID={bookID}
         />
-      </div>
+      )}
+
+      {/* Summary Content section (highlights + notes) */}
+      {book && (
+        <div id="text-editor-section">
+          <Content
+            book={book}
+            bookID={bookID}
+          />
+        </div>
+      )}
     </div>
   );
 }
